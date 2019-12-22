f2a0820d5b22d20d26dfd65fa25f2f9ccd1413c7
benedictbobis <bobis.benedict@gmail.com>
2019-12-22 14:24:46 -0700

    added and setup tailwindcss

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const Home = () => <React.Fragment>Hello World!</React.Fragment>;
+const Home = () => (
+  <React.Fragment>
+    <div className="container mx-auto bg-gray-500">Hello World</div>
+  </React.Fragment>
+);
 
 export default Home;
